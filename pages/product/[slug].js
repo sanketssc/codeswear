35d0211c7212b76d7b891113ba01d9e7c483735c
sanketssc <49080676+sanketssc@users.noreply.github.com>
2022-11-19 18:59:23 +0530

    Added Pages

---
+++ pages/product/[slug].js
@@ -0,0 +1,9 @@
+import { useRouter } from "next/router";
+
+const Post = () => {
+    const router = useRouter()
+    const { slug } = router.query
+    return <p>The slug is : {slug}</p>
+}
+
+export default Post
